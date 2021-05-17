d6614c9f968d528b9e057119c55e256a30325871
Sargarous <sergey.kruglenja@gmail.com>
2021-05-17 23:33:41 +0300

    ex36pvpMode

--- ex36tictactoe.py
+++ ex36tictactoe.py
@@ -4,8 +4,7 @@ import random
 import numpy as np
 from typing import List
 
-# field_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-field_rows = [['X', 'X', 'X'], [4, 5, 6], [7, 8, 9]]
+field_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 def randomizer(min, max):
     return random.randint(min, max)
@@ -65,78 +64,54 @@ def field_setter(matrix: List[List[int]], lst_value: int,
             return field_rows
     return field_rows
 
-
-def all_same(line_of_field):
-    if all(x == line_of_field for x in line_of_field):
-        print(all(x == line_of_field[0] for x in line_of_field))
-        print (line_of_field)
-        if 'X' in line_of_field:
-            return 'X'
-        elif 'O' in line_of_field:
-            return 'O'
-    print(line_of_field)
-# all_same(field_rows[2])
-def victory_check():
-
+def victory_check(lst):
+    winner = ''
     for i in range(0, 3):
-        print(i)
-        check1 = [row[i] for row in field_rows]
-        all_same(check1)
-        # print(f"check1 {check1 == 'X' or 'O'}")
-        check2 = all_same([field_rows[i]])
-        # print(f"check2 {check2 == 'X' or 'O'}")
-
-        # print([row[i] for row in field_rows])
-        # print(field_rows[i])
-        if check1 == 'X' or 'O':
-            return check1
-        elif check2 == 'X' or 'O':
-            return check2
-
-    # check = all_same([field_rows[0][0], field_rows[1][1], field_rows[2][2]])
-    # if check == 'X' or 'O':
-    #     return check
-    # check = all_same([field_rows[0][2], field_rows[1][1], field_rows[2][0]])
-    # if check == 'X' or 'O':
-    #     return check
-
-# playing_field_display()
-#
-print(f"Vi_check_res {victory_check()}")
-
+        if (lst[i][0] == lst[i][1] and lst[i][0] == lst[i][2]):
+            winner = lst[i][0]
+    for j in range(0, 3):
+        if (lst[0][j] == lst[1][j] and lst[0][j] == lst[2][j]):
+            winner = lst[0][j]
+    if (lst[0][0] == lst[1][1] and lst[0][0] == lst[2][2]):
+        winner = lst[0][0]
+    if (lst[0][2] == lst[1][1] and lst[0][2] == lst[2][0]):
+        winner = lst[0][2]
+    return winner
 
 def gameplay(P1, P2, first_move):
-    player_turn = 0
     p1_sign = 'X'
     p2_sign = 'O'
+
     p1 = P1
     p2 = P2
     if first_move == P2:
         p1 = P2
         p2 = P1
     playing_field_display()
-    for i in range(0, 3):
+    for i in range(0, 9):
         if i%2 == 0:
             print(f"{p1}, type number of field where you want to set {p1_sign}")
             position = input("> ")
             field_setter(field_rows, int(position), p1_sign)
             playing_field_display()
-            player_turn += 1
         else:
             print(f"{p2}, type number of field where you want to set {p2_sign}")
             position = input("> ")
             field_setter(field_rows, int(position), p2_sign)
             playing_field_display()
-            player_turn += 1
 
+        winner = victory_check(field_rows)
+        if  winner == p1_sign:
+            print(f"Congrts, {p1} is winner of this match!")
+            print(f"Sad for you, mr.{p2}")
+            break
+        elif winner == p2_sign:
+            print(f"Congrts, {p2} is winner of this match!")
+            print(f"Sad for you, mr.{p1}")
+            break
 
-
-# game_mode = start(0)
-# P1, P2 = pre_game_prep()
-# first_move = who_move_first()
-# gameplay(P1, P2, first_move)
-# field_setter()
-# field_setter(field_rows, 8, 'X')
-
-
-# playing_field_display()
+def
+game_mode = start(0)
+P1, P2 = pre_game_prep()
+first_move = who_move_first()
+gameplay(P1, P2, first_move)
