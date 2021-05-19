dd85aecf173d60d63aec3b44c50f23eabd875c76
Sargarous <sergey.kruglenja@gmail.com>
2021-05-19 11:43:28 +0300

    ex36logic

--- ex36tictactoe.py
+++ ex36tictactoe.py
@@ -6,7 +6,7 @@ from typing import List
 
 field_rows = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
 # field_rows = [['X', 'Z', '3'], ['C', 'H', 'P'], ['7', "1", '9']]
-#return random number in range
+#return random number in range(z, y)
 def randomizer(min, max):
     return random.randint(min, max)
 
@@ -68,11 +68,10 @@ def who_move_first():
     return (first)
 #fancy stuff
 def progress_bar():
-    i = 0
-    while i < 10:
+    for i in range (0, 11):
         time.sleep(randomizer(0, 1))
         print("-", sep = ' ', end = '', flush = True)
-        i += 1
+
 #set O or X in selected position(1-9)
 def field_setter(matrix: List[List[str]], lst_value: str,
                   new_value: str) -> List:
@@ -112,8 +111,8 @@ def gameplay(P1, P2, first_move):
     for i in range(0, 9):
             if i%2 == 0:
                 if(p1 == 'Io' or p1 == 'Hellios'):
-                    position = io_bot(field_rows)
-                    print(f"{p1} strike on {position}")
+                    position = bot_activator()
+                    print(f"{p1} strikes {p1_sign} on {position}")
                 else:
                     print(f"{p1}, type number of field where you want to set {p1_sign}")
                     position = input("> ")
@@ -121,8 +120,8 @@ def gameplay(P1, P2, first_move):
                 playing_field_display()
             else:
                 if(p2 == 'Io' or p2 == 'Hellios'):
-                    position = io_bot(field_rows)
-                    print(f"{p1} strike on {position}")
+                    position = bot_activator()
+                    print(f"{p1} strikes {p1_sign} on {position}")
                 else:
                     print(f"{p2}, type number of field where you want to set {p2_sign}")
                     position = input("> ")
@@ -132,11 +131,13 @@ def gameplay(P1, P2, first_move):
             if  winner == p1_sign:
                 print(f"Congrts, {p1} is winner of this match!")
                 print(f"Sad for you, mr.{p2}")
-                break
+                continue
             elif winner == p2_sign:
                 print(f"Congrts, {p2} is winner of this match!")
                 print(f"Sad for you, mr.{p1}")
-                break
+                continue
+            elif not winner and i == 8:
+                print("Draw!")
 
 #simple randomized action bot
 def io_bot(field_rows):
@@ -146,17 +147,45 @@ def io_bot(field_rows):
             for val in row:
                 if (val == io_turn):
                     return io_turn
-def io_bot(field_rows):
-    while True:
-        hellios_turn = str(randomizer(1, 9))
-        for row in field_rows:
-            for val in row:
-                if (val == hellios_turn):
-                    return hellios_turn
 
+def bot_activator():
+    if game_mode == 2:
+        print('Io')
+        return io_bot(field_rows)
+    elif game_mode == 3:
+        print('Hell')
+        return Hellios_bot(field_rows)
+
+        #advanced bot, smart as hell, sharp as edge, sting like a bee etc.
+def Hellios_bot(field_rows):
+    while True:
+        hellios_turn = win_turn_avaible(field_rows)
+        if hellios_turn:
+            return hellios_turn
+        else:
+            hellios_turn = str(randomizer(1, 9))
+            for row in field_rows:
+                for val in row:
+                    if (val == hellios_turn):
+                        return hellios_turn
+
+#check for two in a row and empty third
+def win_turn_avaible(lst):
+    print('Gocha!!!')
+    win_turn = ''
+    for i in range(0, 3):
+        if (lst[i][0] == lst[i][1] and lst[i][2] != 'X' and lst[i][2] != 'O'):
+            win_turn = lst[i][0]
+    for j in range(0, 3):
+        if (lst[0][j] == lst[1][j] and lst[2][j] != 'X' and lst[2][j] != 'O'):
+            win_turn = lst[2][j]
+    if (lst[0][0] == lst[1][1] and lst[2][2] != 'X' and lst[2][2] != 'O'):
+        win_turn = lst[0][0]
+    if (lst[0][2] == lst[1][1] and [2][0] != 'X' and lst[2][0] != 'O'):
+        win_turn = lst[2][0]
+    return win_turn
 
 # print(io_bot(field_rows))
-
 game_mode = start()
 P1, P2 = pre_game_prep()
 first_move = who_move_first()
