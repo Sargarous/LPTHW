b485fe917418b0b53316a16c17a0189ddfb2bb0f
Sargarous <sergey.kruglenja@gmail.com>
2021-05-22 06:54:43 +0300

    myex39

---
+++ myex39.py
@@ -0,0 +1,19 @@
+regions = {}
+cities = {}
+
+regions['Gomelskaya'] = 'Gomel'
+regions['Brestskaya'] = 'Brest'
+regions['Vitebskaya'] = 'Vitebsk'
+regions['Grodnenskaya'] = 'Grodna'
+
+cities['Gomelskaya'] = 'Shklov'
+cities['Brestskaya'] = 'Borisov'
+cities['Vitebskaya'] = 'Kerch'
+cities['Grodnenskaya'] = 'Bobrujsk'
+
+ print('-' * 10)
+ for citie, region in list(cities.items()):
+     ptint(f"{cities} in {re})
+
+ print('-' * 10)
+ print(from)
