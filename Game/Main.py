8583ad1f9e19b13671660f7bc1b4bfa9a37147d9
Sargarous <sergey.kruglenja@gmail.com>
2021-05-27 23:03:06 +0300

    gane

--- Game/Main.py
+++ Game/Main.py
@@ -26,4 +26,28 @@ class Locations(object):
 
     def __init__(self, arg):
         super(Locations, self).__init__()
-        self.arg = arg
+        rooms = {'first':'1-1'}
+
+class 1-1(Locations):
+    pass
+
+class 1-2(Locations):
+    pass
+
+class 1-3(Locations):
+    pass
+
+class Boss(Character)
+
+print("""Bug ugly troll stand before you and u can smell his rotten breath.
+        It take some thime before he notice you and turn his head in your
+        direction. Seems he studiyng you for some time and you almost can swear
+        that for breef of moment you saw grimm smile on his face.
+        While you draw your sword you notice giant pon on fire behind him and
+        somting familiar in this pot. In next second you realise that is a
+        humans foot and part of head. No time for hesitation, you ither will be
+        a hero or snack for this creature.
+        With a corner of you eyes you saw a swirt blink, you jump backward and
+        was rly surprised by dexterity and speed of creature of this size. Time
+        for final fight!  
+    """)
